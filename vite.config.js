a9e504944cfcd09e57024b33d33661c5ed79233e
bdsah <bdsahin365@gmail.com>
2025-11-20 09:50:09 +0000

    Fix: Remove PWA plugin to fix Vercel build

--- vite.config.js
+++ vite.config.js
@@ -1,53 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'vite.svg'],
-      manifest: {
-        name: 'ScoreTracker - CardCal',
-        short_name: 'CardCal',
-        description: 'A modern React-based score tracking application for board games',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        icons: [
-          {
-            src: 'vite.svg',
-            sizes: 'any',
-            type: 'image/svg+xml'
-          }
-        ]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/api\./i,
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'api-cache',
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 60 * 60 * 24 // 24 hours
-              },
-              cacheableResponse: {
-                statuses: [0, 200]
-              }
-            }
-          }
-        ]
-      },
-      devOptions: {
-        enabled: true,
-        type: 'module'
-      }
-    })
-  ],
+  plugins: [react()],
   envPrefix: 'VITE_',
 })
 
